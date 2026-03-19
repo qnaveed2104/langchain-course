6d2ba79cdd308bd5744121160c9aef7e75139e78
qazi <qazi.naveed@nextgeni.com>
2026-03-19 09:48:35 +0100

    setting env

---
+++ main.py
@@ -0,0 +1,15 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def main():
+    print("Hello from langchain-course!")
+    print(os.getenv("OPENAI_API_KEY"))
+    print(os.getenv("GOOGLE_API_KEY"))
+
+
+if __name__ == "__main__":
+    main()
