088a4cfc6f6111c769ce871478ddc389452f1290
qazi <qazi.naveed@nextgeni.com>
2026-03-22 06:40:34 +0100

    adding agent imports

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+from langchain.agents import create_agent
+from langchain_core.tools import tool
+from langchain_core.messages import SystemMessage
+from langchain_openai import ChatOpenAI
+
+
+
+
 def main():
     print("Hello from langchain-course!")
 
